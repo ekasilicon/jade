fb69685d2814f033a4ff3d2ccda3d2a0dd8604e6
Kimball Germane <krgermane@gmail.com>
2021-08-20 16:40:37 -0600

    Mine multiple nets

--- test/algoexplorer/download.rkt
+++ test/algoexplorer/download.rkt
@@ -1,15 +1,24 @@
 #lang racket/base
-(require net/url
+(require racket/match
+         net/url
          racket/port)
 
+(define nets
+  (hash "mainnet" "https://algoexplorerapi.io/v2/applications/"
+        "testnet" "https://testnet.algoexplorerapi.io/v2/applications/"))
+
+(define current-net-id (make-parameter #f))
+(define current-explorer-base (make-parameter #f))
+
 (define (application app-id)
-  (get-pure-port (string->url (string-append
-                                "https://algoexplorerapi.io/v2/applications/"
-                                app-id))))
+  (get-pure-port (string->url (string-append (current-explorer-base) app-id))))
 
 (module+ main
   (define (download! app-id)
-    (let ([path (build-path "raw" app-id)])
+    (define directory (string-append (current-net-id) "-raw"))
+    (unless (directory-exists? directory)
+      (make-directory directory))
+    (let ([path (build-path directory app-id)])
       (if (file-exists? path)
         (printf "already have ~a\n" app-id)
         (begin
@@ -17,7 +26,14 @@
           (call-with-output-file path
             (λ (op) (copy-port (application app-id) op)))
           (printf "done\n")))))
-
-  (for-each
-   download!
-   (vector->list (current-command-line-arguments))))
+  (match (current-command-line-arguments)
+    [(vector net-id app-ids ...)
+     (cond
+       [(hash-ref nets net-id #f)
+        => (λ (explorer-base)
+             (parameterize ([current-net-id net-id]
+                            [current-explorer-base explorer-base])
+               (for-each download! app-ids)))]
+       [else
+        (displayln "expected <net-id> as first argument; one of...")
+        (for-each (λ (net-id) (printf "  ~a\n" net-id)) (hash-keys nets))])]))
--- test/algoexplorer/extract.rkt
+++ test/algoexplorer/extract.rkt
@@ -1,18 +1,24 @@
 #lang racket/base
-(require json
+(require racket/match
+         json
          net/base64)
 
-(for-each
- (λ (id)
-   (with-handlers ([exn:fail? void])
-     (let ([old-path (build-path "raw" id)]
-           [new-path (build-path "extracted" id)])
-       (unless (file-exists? new-path)
-         (let* ([json (call-with-input-file old-path read-json)]
-                [json (hash-ref json 'params)]
-                [json (hash-ref json 'approval-program)])
-           (call-with-output-file new-path
-             (λ (op) (write-bytes (base64-decode (string->bytes/utf-8 json)) op))))))))
- (directory-list "raw"))
+(match (current-command-line-arguments)
+  [(vector net-id)
+   (let ([raw-path (string-append net-id "-raw")]
+         [ext-path (string-append net-id "-extracted")])
+     (for-each
+      (λ (id)
+        (begin ;with-handlers ([exn:fail? void])
+          (let ([old-path (build-path raw-path id)]
+                [new-path (build-path ext-path id)])
+            (unless (file-exists? new-path)
+              (let* ([json (call-with-input-file old-path read-json)]
+                     [json (hash-ref json 'params)]
+                     [json (hash-ref json 'approval-program)])
+                (call-with-output-file new-path
+                  (λ (op) (write-bytes (base64-decode (string->bytes/utf-8 json)) op))))))))
+      (directory-list raw-path)))])
+
 
 
